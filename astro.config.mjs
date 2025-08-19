a07a62efad79f80dd5e7a882dd6796bcae3c75e1
raymond <raymond@gmail10.com>
2025-08-19 11:29:34 +0900

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
-    adapter: node({
-        mode: 'standalone',
-    }),
+    adapter: cloudflare(),
     output: 'server',
 });
