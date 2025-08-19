4fbe1fb285b53d48c639b4196757c91551626aa2
raymond <raymond@gmail10.com>
2025-08-19 12:01:40 +0900

    change callback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
-    adapter: cloudflare(),
+    adapter: node({
+        mode: 'standalone',
+    }),
     output: 'server',
 });
